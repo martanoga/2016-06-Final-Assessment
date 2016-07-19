e508225306258985982a56ccb25175d05f856c07
Marta Noga <marta.noga@gmail.com>
2016-07-19 15:39:00 +0200

    updated github auth callback to work on localhost

--- config.js
+++ config.js
@@ -2,7 +2,8 @@
 var config = {
     GITHUB_CLIENT_ID : '846665293398efccbc13',
     GITHUB_CLIENT_SECRET : '3707bc64a57afd7377b77e0a84eb6849363ad35d',
-    GITHUB_CALLBACK : 'http://finalassesment.com:3000/auth/callback',
+    //GITHUB_CALLBACK : 'http://finalassesment.com:3000/auth/callback',
+    GITHUB_CALLBACK : 'http://localhost:3000/auth/callback',
     GIPHY_PUBLIC_KEY: 'dc6zaTOxFJmzC',
     GIPHY_BASE_URL: 'api.giphy.com',
     GIPHY_TRENDING_URL: 'http://api.giphy.com/v1/gifs/trending',
